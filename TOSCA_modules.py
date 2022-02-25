82eadad04383eedf38fda9fe5f2d3bcf90aa045d
Daniel <daniel.maksimov@ird.fr>
2022-02-25 16:40:08 +0100

    New main file / TOSCA_modules modified to reflect the whole process

--- TOSCA_modules.py
+++ TOSCA_modules.py
@@ -2,6 +2,9 @@ import sen2chain
 import os
 import time
 from multiprocessing import Process
+
+SEN2CHAIN_DATA_PATH = "/home/daniel/sen2chain_data/"
+
 """
 *****************Downloading of images*****************
 """
@@ -17,40 +20,64 @@ def download_image(start_date: str,
     :param end_date: str, ending date of the time period
     :param tile_list: [str], list of tiles to monitor
     """
-    request = sen2chain.DataRequest(start_date, end_date)
-    req = request.from_tiles(tile_list)
 
-    image_names = list(req['hubs'].keys())
-    print(image_names)
-    # Check the directory to see which files are already there
-    old_file_dict = {}
-    for t in tile_list:
-        path = "/home/maksimov/sen2chain_data/data/L1C/" + t
-        old_file_dict[path] = os.listdir(path)
-    # Download the new files
-    # # New thread created here so the function .join() can be called later
-    # thread_dl = threading.Thread(target=sen2chain.DownloadAndProcess,
-    #                              kwargs={'identifiers': req, 'hubs_limit': {"scihub": 0, "peps": 8}})
-    # thread_dl.start()
-    # thread_dl.join()
+    #First request that shows the number of images to download
+    request = sen2chain.DataRequest(start_date, end_date).from_tiles(tile_list)
 
-    proc = Process(target=sen2chain.DownloadAndProcess,
-                   kwargs={'identifiers': req, 'hubs_limit': {"scihub": 0, "peps": 8}})
-    proc.start()
-    proc.join()
+    image_names = list(request['hubs'].keys())
+    for name in image_names:
+        #Temporary request that contains only one of the images of the main request
+        temp_request = {'aws': {}, 'hubs': {name: request['hubs'][name]}}
+
+        #Image download
+        sen2chain.DownloadAndProcess(temp_request)
+
+        #Image processing
+        process_l1c_to_l2a(name)
+
+        #Index extraction
+        path_to_image = SEN2CHAIN_DATA_PATH + "data/L2A/" + name
+        process_index(path_to_image, ['NDVI','NDWI'])
+
+    return 0
+
+
+
+
+    # # Check the directory to see which files are already there
+    # old_file_dict = {}
+    # for t in tile_list:
+    #     path = "/home/maksimov/sen2chain_data/data/L1C/" + t
+    #     old_file_dict[path] = os.listdir(path)
+    # # Download the new files
+    # # # New thread created here so the function .join() can be called later
+    # # thread_dl = threading.Thread(target=sen2chain.DownloadAndProcess,
+    # #                              kwargs={'identifiers': req, 'hubs_limit': {"scihub": 0, "peps": 8}})
+    # # thread_dl.start()
+    # # thread_dl.join()
+    #
+    # proc = Process(target=sen2chain.DownloadAndProcess,
+    #                kwargs={'identifiers': req, 'hubs_limit': {"scihub": 0, "peps": 8}})
+    # proc.start()
+    # proc.join()
 
     #sen2chain.DownloadAndProcess(identifiers=req, hubs_limit={"scihub": 0, "peps": 8})
 
     #time.sleep(3)
     # Once the download has finished, we can check the given tiles folders to see which files have been added
-    added_files_list = []
-    for t in tile_list:
-        path = "/home/maksimov/sen2chain_data/data/L1C/" + t
-        file_list = os.listdir(path)
-        for f in file_list:
-            if f not in old_file_dict[path]:
-                added_files_list.append(f)
-    return added_files_list
+
+    # added_files_list = []
+    # for t in tile_list:
+    #     path = "/home/maksimov/sen2chain_data/data/L1C/" + t
+    #     file_list = os.listdir(path)
+    #     for f in file_list:
+    #         if f not in old_file_dict[path]:
+    #             added_files_list.append(f)
+
+
+
+    # return added_files_list
+    #
 
 
 
@@ -59,14 +86,15 @@ def download_image(start_date: str,
 """
 
 
-"""
-def process_L1C_to_L2A(tile):
-    t = sen2chain.Tile(tile)
-    l1c = sen2chain.L1cProduct(p.identifier, t.name)
-        try:
-            l1c.process_l2a()
-        except:
-            print("FAILED:", p.identifier)
-        print("PASSED:", p.identifier)
-"""
+def process_l1c_to_l2a(name):
+    l1c = sen2chain.L1cProduct(name)
+    try:
+        l1c.process_l2a()
+    except:
+        print("FAILED:", name)
+    print("PASSED:", name)
+    return 0
 
+
+def process_index(path, list_indexes):
+    return 0
---
+++ main.py
@@ -0,0 +1,3 @@
+from TOSCA_modules import *
+
+download_image("2022-01-01","2022-01-10", ["31TEJ"])
