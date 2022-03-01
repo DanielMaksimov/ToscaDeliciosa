56e5a24b2bcd1ba1f316f0890de9b32f5b86b2bd
DanielMaksimov <daniel.maksimov@ird.fr>
2022-03-01 12:25:02 +0100

    Update with comments

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from TOSCA_modules import *
 
-<<<<<<< HEAD
 abc = download_image("2022-01-07", "2022-01-10", ["31TEJ"])
 #print("The result is:", abc)
 # request = sen2chain.DataRequest("2022-01-01","2022-01-07")
@@ -9,6 +8,5 @@ abc = download_image("2022-01-07", "2022-01-10", ["31TEJ"])
 # print(requests["hubs"].keys())
 
 # sen2chain.DownloadAndProcess(identifiers=requests, hubs_limit={"scihub": 0, "peps": 8})
-=======
+
 download_image("2022-01-01","2022-01-10", ["31TEJ"])
->>>>>>> 82eadad04383eedf38fda9fe5f2d3bcf90aa045d
