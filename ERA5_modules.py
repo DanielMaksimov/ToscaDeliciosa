bc5785eea357c805aaa7645c57e77d0fd403e688
DanielMaksimov <daniel.maksimov@ird.fr>
2022-04-13 15:07:48 +0200

    Era5 modules added

--- ERA5_modules.py
+++ ERA5_modules.py
@@ -1,1 +1,20 @@
 import netCDF4 as nc
+import cdsapi
+
+c = cdsapi.Client()
+
+c.retrieve(
+    'reanalysis-era5-land-monthly-means',
+    {
+        'product_type': 'monthly_averaged_reanalysis',
+        'variable': 'skin_temperature',
+        'year': '2021',
+        'month': '12',
+        'time': '00:00',
+        'area': [
+            8, -75, -34,
+            -30,
+        ],
+        'format': 'netcdf',
+    },
+    '/home/maksimov/ERA5_download.nc')
