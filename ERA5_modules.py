ffa764b460b2712b1de9c319f189b9a25312e7d2
DanielMaksimov <daniel.maksimov@ird.fr>
2022-04-14 17:08:19 +0200

    Added nc.Dataset handling. Climate data acquisition and processing. Proceessing needs some tweaking to obtain a pd.DataFrame in the end

--- ERA5_modules.py
+++ ERA5_modules.py
@@ -1,20 +1,38 @@
 import netCDF4 as nc
 import cdsapi
+import pandas as pd
 
-c = cdsapi.Client()
+def get_climate_data(year: [str],
+                     month: [str],
+                     path: str) -> object:
+    """
+    Downloads the climate data and returns a panda DataFrame
+    """
+    c = cdsapi.Client()
+    c.retrieve(
+        'reanalysis-era5-land-monthly-means',
+        {
+            'product_type': 'monthly_averaged_reanalysis',
+            'variable': 'skin_temperature',
+            'year': '2021',
+            'month': '12',
+            'time': '00:00',
+            'area': [
+                8, -75, -34,
+                -30,
+            ],
+            'format': 'netcdf',
+        },
+        '/home/maksimov/ERA5_download.nc')
+    return 0
 
-c.retrieve(
-    'reanalysis-era5-land-monthly-means',
-    {
-        'product_type': 'monthly_averaged_reanalysis',
-        'variable': 'skin_temperature',
-        'year': '2021',
-        'month': '12',
-        'time': '00:00',
-        'area': [
-            8, -75, -34,
-            -30,
-        ],
-        'format': 'netcdf',
-    },
-    '/home/maksimov/ERA5_download.nc')
+
+def process_climate_data(path: str) -> pd.DataFrame:
+    dataset = nc.Dataset(path)
+    times = dataset.variables['time']
+    skin_temp = dataset.variables['skt']
+    new_times = nc.num2date(times[:], times.units)
+    pd_df = pd.Series(skin_temp[:, 0], index=new_times)
+    return pd_df
+
+process_climate_data('/home/maksimov/ERA5_download.nc')
